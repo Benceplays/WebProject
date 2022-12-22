73f578a124ec94ee8e2749114bb1b165c87a3f54
Németh Csaba Bence <71324001+Benceplays@users.noreply.github.com>
2022-12-22 15:25:29 +0100

    A design az nem lett jó de mindent megtettem

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 var http = require('http');
 var fs = require('fs');
-var index = fs.readFileSync('index.html');
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end(index);
+http.createServer((req, res) => {
+    console.log(req.url)
+    if (req.url === "/") {
+        fs.readFile('index.html', (err, html) => {
+            if (err) {
+                throw err;
+            }
+            res.setHeader('Content-type', 'text/html');
+            res.write(html);
+            res.statusCode = 200;
+            res.end();
+        });
+    }
+    else if (req.url == '/style.css') {
+        res.setHeader('Content-type', 'text/css');
+        res.write(fs.readFileSync('style.css'));
+        res.end();
+    } else {
+        res.write("invalid request")
+        res.end();
+    }
 }).listen(8090);
