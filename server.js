388ae22f1a68b11e068e43e1a42962fec024e0ba
Script Devil <71324001+Benceplays@users.noreply.github.com>
2023-01-19 18:18:15 +0100

    Update server.js

--- server.js
+++ server.js
@@ -23,3 +23,22 @@ http.createServer((req, res) => {
         res.end();
     }
 }).listen(8090);
+
+//Ez lesz az adatbázis
+/*
+const mysql = require('mysql');
+
+const con = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'wildem'
+});
+
+con.query('SELECT * FROM login', (err,rows) => {
+    if(err) throw err;
+        rows.forEach(function(row){
+            console.log(`${row.username} lives in ${row.email}`);
+        });
+});
+*/
