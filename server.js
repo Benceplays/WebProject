b3bd8ae64b73b85b0067544b70c6f79bb5d3b396
Németh Csaba Bence <71324001+Benceplays@users.noreply.github.com>
2023-01-24 09:35:31 +0100

    par cucc

--- server.js
+++ server.js
@@ -1,28 +1,26 @@
-var http = require('http');
-var fs = require('fs');
+var express = require('express');
+var path = require('path');
+var app = express();
 
-http.createServer((req, res) => {
-    console.log(req.url)
-    if (req.url === "/") {
-        fs.readFile('index.html', (err, html) => {
-            if (err) {
-                throw err;
-            }
-            res.setHeader('Content-type', 'text/html');
-            res.write(html);
-            res.statusCode = 200;
-            res.end();
-        });
-    }
-    else if (req.url == '/style.css') {
-        res.setHeader('Content-type', 'text/css');
-        res.write(fs.readFileSync('style.css'));
-        res.end();
-    } else {
-        res.write("invalid request")
-        res.end();
-    }
-}).listen(8090);
+app.use('/jsFiles', express.static(__dirname + '/js'));
+app.use('/cssFiles', express.static(__dirname + '/css'));
+app.use('/image', express.static(__dirname + '/images'));
+app.get('/', function(req, resp) {
+    resp.sendFile('index.html', {root: path.join(__dirname, './')});
+});
+app.post('/stop_button', function (req, res) {
+    const data = { name: 'John', age: 30 };
+    res.json(data);
+});
+app.post('/home_team_dropdown', function (req, res) {
+    console.log('valasztva');
+});
+app.post('/away_team_dropdown', function (req, res) {
+    console.log('valasztvaaway');
+});
+app.listen(1337, function(){
+    console.log("fut");
+});
 
 //Ez lesz az adatbázis
 /*
@@ -40,20 +38,5 @@ con.query('SELECT * FROM login', (err,rows) => {
         rows.forEach(function(row){
             console.log(`${row.username} lives in ${row.email}`);
         });
-});
-*/
-//És így lesz a szerver
-/*
-var express = require('express');
-var path = require('path');
-var app = express();
-
-app.use('/cssFiles', express.static(__dirname + '/assets'));
-app.get('/', function(req, resp) {
-    resp.sendFile('index.html', {root: path.join(__dirname, './')});
-});
-
-app.listen(1337, function(){
-    console.log("fut");
 });
 */
